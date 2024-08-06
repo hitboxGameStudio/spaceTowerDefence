7f286177d20deac6b0245e5ae97775f4a6f5a950
ahmetbozkurt <ahmbozk@gmail.com>
2024-08-06 19:08:57 +0300

    samplescene

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float speed = 6.0f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        if (controller.isGrounded)
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            moveDirection = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+            }
+        }
+
+        // Yer çekimi
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        // Hareketi uygula
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
